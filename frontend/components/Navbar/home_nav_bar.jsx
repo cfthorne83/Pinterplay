e5c96ef4057ae3e24d3aaa7cb7d60d5dc0e929f9
cthorne <cfthorne83@gmail.com>
2021-01-23 16:03:15 -0500

    spaced out home navbar

--- frontend/components/Navbar/home_nav_bar.jsx
+++ frontend/components/Navbar/home_nav_bar.jsx
@@ -7,26 +7,34 @@ class HomeNavBar extends React.Component {
     render() {
 
         return (
-            <div>
-                <ul className='home-nav-bar'>
-                    <li><img className='logo' src="https://seeklogo.com/images/P/pinterest-badge-logo-82C89A5E42-seeklogo.com.png" alt="myPin Logo" /></li>
-                    <li>Home</li>
-                    <li>Today</li>
-                    <li>Following</li>
-                    <li className='searchbar'><input type="text" /></li>
-                    <li>
-                        <img className='update-bell' src="https://www.flaticon.com/svg/vstatic/svg/1827/1827347.svg?token=exp=1611428875~hmac=a2ec2aa117173d2d676fb56775dabbb9" alt="update bell"/>
-                    </li>
-                    <li>
-                        <img className='update-bell' src="https://www.flaticon.com/svg/vstatic/svg/684/684849.svg?token=exp=1611428751~hmac=8da5ce304e20dd595c1c7beaea8b919e" alt="update bell"/>
-                    </li>
-                    <li>
-                        <LogoutDropdown
-                        logout={this.props.logout}
-                        />
-                    </li>
-                </ul>
-            </div>
+            <header className='home-nav-bar'>
+                    <img className='home-logo' src="https://seeklogo.com/images/P/pinterest-badge-logo-82C89A5E42-seeklogo.com.png" alt="myPin Logo" />
+
+                <nav>
+                    <ul className='nav-links'>
+                        <li>Home</li>
+                        <li>Today</li>
+                        <li>Following</li>
+                    </ul>
+                </nav>
+
+                <form>
+                    <input className='searchbar' type="text" />
+                </form>
+
+                <nav>
+                    <ul className='nav-dropdowns'>
+                        <li><img className='update-bell' src="https://www.flaticon.com/svg/vstatic/svg/1827/1827347.svg?token=exp=1611428875~hmac=a2ec2aa117173d2d676fb56775dabbb9" alt="update bell" /></li>
+                        <li><img className='inbox-link' src="https://www.flaticon.com/svg/vstatic/svg/684/684849.svg?token=exp=1611428751~hmac=8da5ce304e20dd595c1c7beaea8b919e" alt="inbox link" /></li>
+                        <li></li>
+                        <li>
+                            <LogoutDropdown
+                            logout={this.props.logout}
+                            />
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
--- frontend/components/Navbar/splash_nav_bar.jsx
+++ frontend/components/Navbar/splash_nav_bar.jsx
@@ -5,7 +5,8 @@ class SplashNavBar extends React.Component {
     render() {
         return (
 
-            <div className='header'>
+            
+            <header>
                 <ul className='header-logo'>
                     <li><img className='logo' src="https://seeklogo.com/images/P/pinterest-badge-logo-82C89A5E42-seeklogo.com.png" alt="myPin Logo" /></li>
                     <li className='app-name'>myPin</li>
@@ -23,9 +24,8 @@ class SplashNavBar extends React.Component {
                         <button className='login' onClick={() => this.props.openModal('login')}>Log in</button>
                         <button className='signup' onClick={() => this.props.openModal('signup')}>Sign up</button>
                     </ul>
-
                 </nav>
-            </div>
+            </header>
 
         );
     }
