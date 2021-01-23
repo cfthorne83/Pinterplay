a8ec266e245455b826fb56359039e1a8acb9230f
cthorne <cfthorne83@gmail.com>
2021-01-23 11:52:03 -0500

    delete extraneous files

--- frontend/components/Navbar/home_nav_bar.jsx
+++ frontend/components/Navbar/home_nav_bar.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
 import LogoutDropdown from './logout_drop_down';
-import NavLinks from './nav_links'
 
 class HomeNavBar extends React.Component {
 
     render() {
-        // return <NavLinks
-        //     openModal={this.props.openModal}
-        //     logout={this.props.logout}
-        //     currentUser={this.props.currentUser}
-        // />
+
         return <LogoutDropdown
             openModal={this.props.openModal}
             logout={this.props.logout}
--- frontend/components/Navbar/splash_nav_bar.jsx
+++ frontend/components/Navbar/splash_nav_bar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavLinks from './nav_links'
 
 class SplashNavBar extends React.Component {
 
@@ -13,20 +12,18 @@ class SplashNavBar extends React.Component {
                 </ul>
 
                 <nav className='header-nav-lists'>
+
                     <ul className='mid-links'>
                         <li>About</li>
                         <li>Business</li>
                         <li>Blog</li>
                     </ul>
-                    {/* <NavLinks
-                        openModal={this.props.openModal}
-                        logout={this.props.logout}
-                        currentUser={this.props.currentUser}
-                    /> */}
+                 
                     <ul className='nav-links'>
                         <button className='login' onClick={() => this.props.openModal('login')}>Log in</button>
                         <button className='signup' onClick={() => this.props.openModal('signup')}>Sign up</button>
                     </ul>
+
                 </nav>
             </div>
 
@@ -35,41 +32,3 @@ class SplashNavBar extends React.Component {
 }
 
 export default SplashNavBar;
-
-// import React from 'react';
-// import NavLinks from './nav_links'
-
-// class SplashNavBar extends React.Component {
-//     constructor(props){
-//         super(props);
-
-//     }
-
-//     render() {
-//         return (
-
-//             <div className='header'>
-//                 <ul className='header-logo'>
-//                     <li><img className='logo' src="https://seeklogo.com/images/P/pinterest-badge-logo-82C89A5E42-seeklogo.com.png" alt="myPin Logo" /></li>
-//                     <li className='app-name'>myPin</li>
-//                 </ul>
-
-//                 <nav className='header-nav-lists'>
-//                     <ul className='mid-links'>
-//                         <li>About</li>
-//                         <li>Business</li>
-//                         <li>Blog</li>
-//                     </ul>
-
-//                     <ul className='nav-links'>
-//                         <button className='login' onClick={() => this.props.openModal('login')}>Log in</button>
-//                         <button className='signup' onClick={() => this.props.openModal('signup')}>Sign up</button>
-//                     </ul>
-//                 </nav>
-//             </div>
-
-//         );
-//     }
-// }
-
-// export default SplashNavBar;
