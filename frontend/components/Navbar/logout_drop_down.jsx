a21c06ef0a0d4dbe3bad304b5a681477e1b1aeb1
cthorne <cfthorne83@gmail.com>
2021-01-23 09:55:35 -0500

    work on dropdown

--- frontend/components/Navbar/logout_drop_down.jsx
+++ frontend/components/Navbar/logout_drop_down.jsx
@@ -29,20 +29,31 @@ class LogoutDropdown extends React.Component{
 
         return (
             <button 
-                className='dropdown-button'
+                className='dropdown-outer'
                 onClick={this.handleClick} 
+                // onBlur={this.handleBlur}
                 >
     
-                    <span>dropdown</span>
+                    <span>V</span>
                     {this.state.show && (
-                        <div className='dropdown-contents'>
-                            <ul>
+                        <div className='dropdown-inner'>
+                            <ul >
+                                <li>Add another account</li>
+                                <li>Unlock business tools</li>
                                 <li>Settings</li>
-                                <li><input type="submit" value='Log out' onClick={this.props.logout} /></li>
+                                <li>Tune your home feed</li>
+                                <li>Install the Chrome app</li>
+                                <li>Get help</li>
+                                <li>See the terms and privacy</li>
+                                <li><input 
+                                        type="submit" 
+                                        value='Log out' 
+                                        onClick={this.props.logout} /></li>
                             </ul>
                         </div>
                     )}
             </button>
+            
         )
     }
 }
