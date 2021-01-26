4fae22b0355b0ae8f3d9d15ecf7ac8ba569fefb2
cthorne <cfthorne83@gmail.com>
2021-01-26 17:37:54 -0500

    reseed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# Board.create(title: "board1", description: "test1", user_id: 11)
-# Board.create(title: "board2", description: "test2", user_id: 11)
-# Board.create(title: "board3", description: "test3", user_id: 11)
-
 demo_user = User.create(email: "demoEmail", password: "123456")
 
 Board.create(title: "title1", description: "description1", user_id: demo_user.id)
 Board.create(title: "title2", description: "description2", user_id: demo_user.id)
 Board.create(title: "title3", description: "description3", user_id: demo_user.id)
+
+
+# Board.create!(title: "title1", description: "description1", user_id: demo_user.id)
+# Board.create!(title: "title2", description: "description2", user_id: demo_user.id)
+# Board.create!(title: "title3", description: "description3", user_id: demo_user.id)
