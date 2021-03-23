630113705dcbfc3ceb8abbcf81048dd2d505c715
cthorne <cfthorne83@gmail.com>
2021-03-23 14:34:15 -0400

    start pin ajax

--- frontend/util/pin_api_util.js
+++ frontend/util/pin_api_util.js
@@ -1,7 +1,35 @@
-import { $CombinedState } from "redux"
 
 export const fetchPins = () => {
     return $.ajax({
-        
+        url: '/api/pins'
+    })
+}
+
+export const fetchPin = (pinId) => {
+    return $.ajax({
+        url: `/api/pins/${pinId}`
+    })
+}
+
+export const createBoard = (board) => {
+    return $.ajax({
+        url: '/api/boards',
+        method: 'POST',
+        data: { board }
+    })
+}
+
+export const updateBoard = (board) => {
+    return $.ajax({
+        url: `/api/boards/${board.id}`,
+        method: 'PATCH',
+        data: { board }
+    })
+}
+
+export const deleteBoard = (boardId) => {
+    return $.ajax({
+        url: `/api/boards/${boardId}`,
+        method: "DELETE"
     })
 }
