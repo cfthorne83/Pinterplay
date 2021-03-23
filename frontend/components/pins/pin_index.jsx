84cb836dd351eb8938948c9eb79b0772cfd66a00
cthorne <cfthorne83@gmail.com>
2021-03-23 15:02:23 -0400

    add pin index

---
+++ frontend/components/pins/pin_index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class PinIndex extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPins();
+    }
+
+    render() {
+
+        let pins = this.props.pins.map(pin => {
+            return <Link to={`/pins/${pin.id}`}><li key={`${pin.id}`}>{pin.title}</li></Link>
+        })
+        
+        return (
+            
+                <ul className='pin-index'>
+                        {pins}
+                </ul>  
+    
+        )
+    }
+}
+
+export default PinIndex;
