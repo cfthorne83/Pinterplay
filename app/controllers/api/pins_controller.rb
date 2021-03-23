45265de3adfc485b0211cd6cdbcb9ef8444f448e
cthorne <cfthorne83@gmail.com>
2021-03-23 09:22:56 -0400

    outline pins controller

--- app/controllers/api/pins_controller.rb
+++ app/controllers/api/pins_controller.rb
@@ -1,9 +1,32 @@
 class PinsController < ApplicationController
 
+    before_action :ensure_logged_in
+
     def index
         @pins = Pin.all
         render :index
     end
+
+    def show 
+
+    end
+
+    def create
+    end
+
+    def edit
+    
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
 
     def pin_params
         params.require(:pin).permit(:title, :user_id)
