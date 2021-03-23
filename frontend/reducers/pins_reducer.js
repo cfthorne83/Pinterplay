20820a5f617d4d79cbbb5c4207a800b4d9c6d306
cthorne <cfthorne83@gmail.com>
2021-03-23 14:54:17 -0400

    finish pins reducer

--- frontend/reducers/pins_reducer.js
+++ frontend/reducers/pins_reducer.js
@@ -1,24 +1,24 @@
 import {
-    RECEIVE_ALL_BOARDS,
-    RECEIVE_BOARD,
-    REMOVE_BOARD,
-} from '../actions/board_actions';
+    RECEIVE_ALL_PINS,
+    RECEIVE_PIN,
+    REMOVE_PIN,
+} from '../actions/pin_actions';
 
-const boardsReducer = (state = {}, action) => {
+const pinsReducer = (state = {}, action) => {
     Object.freeze(state);
 
     switch (action.type) {
-        case RECEIVE_ALL_BOARDS:
-            return action.boards;
-        case RECEIVE_BOARD:
-            return Object.assign({}, state, { [action.board.id]: action.board });
-        case REMOVE_BOARD:
+        case RECEIVE_ALL_PINS:
+            return action.pins;
+        case RECEIVE_PIN:
+            return Object.assign({}, state, { [action.pin.id]: action.pin });
+        case REMOVE_PIN:
             let newState = Object.assign({}, state);
-            delete newState[action.boardId];
+            delete newState[action.pinId];
             return newState;
         default:
             return state;
     }
 }
 
-export default boardsReducer;
+export default pinsReducer;
