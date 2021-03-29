33db5836cf0fe20b3186b57888823048eb9ede3e
cthorne <cfthorne83@gmail.com>
2021-03-29 14:25:58 -0400

    hide input

--- frontend/components/profile/edit_photo_form.jsx
+++ frontend/components/profile/edit_photo_form.jsx
@@ -4,11 +4,39 @@ class EditPhotoForm extends React.Component{
     constructor(props){
         super(props);
 
+        this.handleClick = this.handleClick.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleClick() {
+        let input = document.querySelector(".profile-photo__input")
+
+        input.click();
+    }
+
+    handleInput(e) {
+
+        console.log(e.target.files[0]);
+        // let reader = new FileReader();
+        // reader.readAsDataURL(files[0]);
+
+        // reader.onload = () => {
+        //     image = reader.result;
+        //     // that.setState({ image_url: image });
+        //     console.log(image);
+        // }
     }
 
     render() {
         return (
-            <h1>edit photo</h1>
+            <div>
+                <h1>edit photo</h1>
+                <input 
+                    onChange={this.handleInput}
+                    type="file" 
+                    className="profile-photo__input"/>
+                <button onClick={this.handleClick}>Choose photo</button>
+            </div>
         )
     }
 }
