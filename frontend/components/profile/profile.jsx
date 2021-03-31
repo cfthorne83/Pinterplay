de6e32e6371a9c451b09ff02c43dd2111d9b8dda
cthorne <cfthorne83@gmail.com>
2021-03-30 22:30:37 -0400

    add name to profile

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -9,16 +9,39 @@ class Profile extends React.Component{
         super(props);
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.capitalize = this.capitalize.bind(this);
+        this.name = this.name.bind(this);
     }
 
     handleSubmit(e){
         this.props.openModal('createBoard');
     }
 
+    capitalize(name) {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
+    name() {
+        const { currentUser } = this.props;
+
+        if (currentUser.fname && currentUser.lname) {
+            return (
+                <h2>
+                    {this.capitalize(currentUser.fname)}&nbsp;
+                    {this.capitalize(currentUser.lname)}
+                </h2>
+            )
+        } else {
+            return (
+                <h2>{currentUser.email}</h2>
+            )
+        }
+    }
+
     render() {
 
         const { currentUser } = this.props;
-
+        
         if (!this.props.currentUser) {
             return <Redirect to='/'/>
         } 
@@ -30,7 +53,12 @@ class Profile extends React.Component{
                     <h1 className='initial-container'>
                         <p>{currentUser.email[0].toUpperCase()}</p>
                     </h1>
-                    <h2>{currentUser.email}</h2>
+                    {/* <h2>
+                        {this.capitalize(currentUser.fname)}&nbsp;
+                        {this.capitalize(currentUser.lname)}
+                    </h2> */}
+                    <h2>{this.name()}</h2>
+                    <h3>@{currentUser.username}</h3>
                 </section>
 
                 <section className='mid'>
