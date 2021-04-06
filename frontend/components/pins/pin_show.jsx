ab583419641d0267b52491bd2199f798572719f9
cthorne <cfthorne83@gmail.com>
2021-04-06 17:51:31 -0400

    start style pin show

--- frontend/components/pins/pin_show.jsx
+++ frontend/components/pins/pin_show.jsx
@@ -30,18 +30,33 @@ class PinShow extends React.Component {
         
         return (
             <div className="pin-show">
-                <button onClick={this.handleGoBack}>
-                    <img src="/images/back-arrow2.png" alt=""/>
+                <button 
+                    className="pin-show__back"
+                    onClick={this.handleGoBack}>
+                    <img 
+                        className="pin-show__back-img"
+                        src="/images/back-arrow2.png" 
+                        alt="back-arrow"
+                        />
                 </button>
 
-                <h1>Pin show</h1>
-                <h1>{this.props.pin.title}</h1>
-                <img src={this.props.pin.image_url} alt=""/>
-                <img 
-                    onClick={()  => {this.props.openModal("editPin", this.props.pin.id)}} 
-                    src="https://static.thenounproject.com/png/384290-200.png" 
-                    alt="edit-form-link"
-                    className='edit-pin-modal'/>
+                <div className="pin-show__inner">
+                    <img 
+                        className="pin-show__img"
+                        src={this.props.pin.image_url} 
+                        alt="pin-image"/>
+
+                    <div className="pin-show__text">
+                        
+                        <img
+                            className="pin-show__edit-modal"
+                            onClick={()  => {this.props.openModal("editPin", this.props.pin.id)}} 
+                            src="https://static.thenounproject.com/png/384290-200.png" 
+                            alt="edit-form-link"/>
+                        <h1>{this.props.pin.title}</h1>
+                    </div>
+                </div>
+
             </div>
         )
     }
