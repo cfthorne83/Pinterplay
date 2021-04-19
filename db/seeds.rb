c93fbadf22e64112774f4f2d1714d6a9fa55e299
cthorne <cfthorne83@gmail.com>
2021-04-19 14:48:20 -0400

    modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,38 +16,37 @@ demo_user = User.create(
                         password: "123456"
                     )
 
-Board.create([
-    {
+board = Board.create(
         title: "Fire", 
         description: "Fire!", 
         user_id: demo_user.id   
-    },
-    {
-        title: "Water", 
-        description: "Water!", 
-        user_id: demo_user.id   
-    },
-    {
-        title: "Earth", 
-        description: "Earth!", 
-        user_id: demo_user.id   
-    },
-    {
-        title: "Air", 
-        description: "Air!", 
-        user_id: demo_user.id   
-    },
-    {
-        title: "Ice", 
-        description: "Ice!", 
-        user_id: demo_user.id   
-    }
-])
+    )
+#     {
+#         title: "Water", 
+#         description: "Water!", 
+#         user_id: demo_user.id   
+#     },
+#     {
+#         title: "Earth", 
+#         description: "Earth!", 
+#         user_id: demo_user.id   
+#     },
+#     {
+#         title: "Air", 
+#         description: "Air!", 
+#         user_id: demo_user.id   
+#     },
+#     {
+#         title: "Ice", 
+#         description: "Ice!", 
+#         user_id: demo_user.id   
+#     }
+# ])
 
 # demo_user = User.create(username: "Andy", email: "andy@email.com", password: "password")
 
-pin = Pin.create(title: "test")
+pin = Pin.create(title: "test", board_id: board.id)
 
-file = open('https://mypin-ct-dev.us-east-1.amazonaws.com/https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+# file = open('https://mypin-ct-dev.us-east-1.amazonaws.com/https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
 
-pin.photo.attach(io: file, filename: 'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+# pin.photo.attach(io: file, filename: 'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
