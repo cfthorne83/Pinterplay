2601e408201deeae7987f81096fd83f883ef0cb9
cthorne <cfthorne83@gmail.com>
2021-04-20 17:07:20 -0400

    seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,6 +51,6 @@ pin = Pin.create(title: "test", board_id: board.id, user_id: demo_user.id)
 # file = open("https://hyperallergic.com/wp-content/uploads/2021/04/The-GREEN-Rendering_captioned-1200x900.jpeg?crop=1")
 # debugger
 # file = File.open("/Users/christopherthorne/Desktop/fran-hogan-gMca5DJ25Qo-unsplash.jpg")
-https://images.unsplash.com/photo-1487139975590-b4f1dce9b035?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80
 
-pin.photo.attach(io: file, filename: 'fran-hogan-gMca5DJ25Qo-unsplash.jpg')
+file = open('https://images.unsplash.com/photo-1487139975590-b4f1dce9b035?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80')
+pin.photo.attach(io: file, filename: 'test.jpg')
