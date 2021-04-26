49cef1f8e3796fe6692905772f022a13597d2833
cthorne <cfthorne83@gmail.com>
2021-04-26 15:02:35 -0400

    finish seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ nums = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", 
 
 arr = []
 (0..4).each do |board_i|
-    (0..12).each do |i| 
+    (0..11).each do |i| 
 
         if (board_i < 3) && (i > 9)
             next
@@ -57,12 +57,10 @@ arr = []
             title = board.title + " " + nums[i]
             uri = "https://mypin-seeds.s3.amazonaws.com/#{initial}.jpg"
 
-            arr << [title]
-    
-            # file = URI.open(uri)
-            # pin = Pin.create(title: title, board_id: board.id, user_id: demo_user.id)
-            # pin.photo.attach(io: file, filename: '#{initial}.jpg')
-            # pin.save
+            file = URI.open(uri)
+            pin = Pin.create(title: title, board_id: board.id, user_id: demo_user.id)
+            pin.photo.attach(io: file, filename: '#{initial}.jpg')
+            pin.save
         end
     end
 end
