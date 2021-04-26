6355a7474c45b53db80ac7ee9b35e5dcb662557b
cthorne <cfthorne83@gmail.com>
2021-04-26 14:58:05 -0400

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,21 +45,26 @@ gold = Board.create(
 boards = [blue, light, pink, dark, gold]
 nums = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen"]
 
+arr = []
 (0..4).each do |board_i|
-    (0..9).each do |i| 
+    (0..12).each do |i| 
 
-        if board_i < 3 && i > 9
+        if (board_i < 3) && (i > 9)
             next
         else
             board = boards[board_i]
             initial = board.title[0].downcase + i.to_s
             title = board.title + " " + nums[i]
             uri = "https://mypin-seeds.s3.amazonaws.com/#{initial}.jpg"
+
+            arr << [title]
     
-            file = URI.open(uri)
-            pin = Pin.create(title: title, board_id: board.id, user_id: demo_user.id)
-            pin.photo.attach(io: file, filename: '#{initial}.jpg')
-            pin.save
+            # file = URI.open(uri)
+            # pin = Pin.create(title: title, board_id: board.id, user_id: demo_user.id)
+            # pin.photo.attach(io: file, filename: '#{initial}.jpg')
+            # pin.save
         end
     end
 end
+debugger
+arr2 = arr
