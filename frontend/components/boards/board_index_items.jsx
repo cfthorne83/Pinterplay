21749c8961dc31d442fbdfdc91030c7010a46796
cthorne <cfthorne83@gmail.com>
2021-04-27 10:57:33 -0400

    finish board image preview

--- frontend/components/boards/board_index_items.jsx
+++ frontend/components/boards/board_index_items.jsx
@@ -31,12 +31,12 @@ class BoardIndexItem extends React.Component {
         const {board} = this.props;
         const pins = board.pins;
         
-            pinDivs = [0, 1, 2].map( i => {
+        const pinDivs = [0, 1, 2].map( i => {
                 if (pins[i]){
                     return (
                         <img 
                             className={`p${i}`}
-                            src={pins[i].image_url} 
+                            src={this.state.pinsImages[i]} 
                             alt=""/>
                     );
                 } else {
@@ -45,20 +45,17 @@ class BoardIndexItem extends React.Component {
                     );
                 }
             });
-         
         return (
             <li key={board.id}>
                 <Link key={`${board.id}`} to={`/boards/${board.id}`}>
              
-                    {/* <div className="outer">
+                    <div className="outer">
                             {pinDivs[0]}
                         <div className="inner">
                             {pinDivs[1]}
                             {pinDivs[2]}
                         </div>
-                    </div> */}
-                    {/* <h1>{pins[0].image_url}</h1> */}
-                    {/* <img src={`${pins[0].image_url}`} alt=""/> */}
+                    </div>
 
                     <h1>{board.title}</h1>
                     <h2>{pins.length} Pins</h2>
