1bf9b7d28cd23bfe0dffa5725f0a8324e71b94db
cthorne <cfthorne83@gmail.com>
2021-05-15 10:35:22 -0400

    def create/destroy in friendships controller[

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -1,12 +1,19 @@
-class Api::BoardsController < ApplicationController
+class Api::FriendshipsController < ApplicationController
 
     before_action: :ensure_logged_in
+    before_action: :find_user
     
     def create 
-
+        current_user.follow(@user)
     end
 
     def destroy
+        current_user.unfollow(@user)
+    end
 
+    private 
+
+    def find_user 
+        @user = user.find(params[:user_id])
     end
 end
