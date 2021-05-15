4c0823c285fd608b8b72f2d11d3582b9ac04dd4d
cthorne <cfthorne83@gmail.com>
2021-05-15 09:20:51 -0400

    add folow functions to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,30 @@ class User < ApplicationRecord
         foreign_key: :follower_id, 
         dependent: :destroy
 
+    has_many :passive_friendships, 
+        class_name: :Friendship,
+        foreign_key: :followed_id,
+        dependent: :destroy
+
     has_many :following, 
         through: :active_friendships,
         source: :followed
+
+    has_many :followers, 
+        through: :passive_friendships,
+        source: :follower
+
+    def follow(user)
+        active_friendships.create(follower_id: user_id)
+    end
+
+    def unfollow(user)
+        active_friendships.find_by(follower_id: user_id).destroy
+    end
+
+    def following?(user)
+        following.include?(user)    
+    end
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
