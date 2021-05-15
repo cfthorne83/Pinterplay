4a92b4764525c75265d900b0306071abccf1ab42
cthorne <cfthorne83@gmail.com>
2021-05-15 10:10:04 -0400

    follows backend working

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,9 +3,6 @@ class Friendship < ApplicationRecord
     validates :follower_id, presence: true
     validates :followed_id, presence: true
 
-    belongs_to :follower, 
-        class_name: :User
-
-    belongs_to :followed,
-        class_name: :User
+    belongs_to :follower, class_name: "User"
+    belongs_to :followed, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,30 +22,25 @@ class User < ApplicationRecord
     has_many :boards
     has_many :pins
 
-    has_many :active_friendships,
-        class_name: :Friendship,
-        foreign_key: :follower_id, 
+    # has_many :active_friendships,
+    #     class_name: :Friendship,
+    #     foreign_key: :follower_id, 
+    #     dependent: :destroy
+    has_many :active_friendships, class_name: "Friendship", foreign_key: "follower_id",
         dependent: :destroy
 
-    has_many :passive_friendships, 
-        class_name: :Friendship,
-        foreign_key: :followed_id,
+    has_many :passive_friendships, class_name: "Friendship", foreign_key: "followed_id",
         dependent: :destroy
 
-    has_many :following, 
-        through: :active_friendships,
-        source: :followed
-
-    has_many :followers, 
-        through: :passive_friendships,
-        source: :follower
+    has_many :following, through: :active_friendships, source: :followed
+    has_many :followers, through: :passive_friendships, source: :follower
 
     def follow(user)
-        active_friendships.create(follower_id: user.id)
+        active_friendships.create(followed_id: user.id)
     end
 
     def unfollow(user)
-        active_friendships.find_by(follower_id: user.id).destroy
+        active_friendships.find_by(followed_id: user.id).destroy
     end
 
     def following?(user)
