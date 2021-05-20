33112b9f2717ab4e8daddbdccb00402bedabf6e9
cthorne <cfthorne83@gmail.com>
2021-05-20 16:51:26 -0400

    delete dead code searchbar.jsx

--- frontend/components/navbar/searchbar.jsx
+++ frontend/components/navbar/searchbar.jsx
@@ -27,25 +27,6 @@ class Searchbar extends React.Component{
     }
 
     render() {
-
-        if (!this.props.pins) return null;
-        const pins = Object.values(this.props.pins).filter( pin => {
-                if (this.state.searchTerm === ""){
-                    return pin
-                } else if ( pin.title.toLowerCase().includes( this.state.searchTerm.toLowerCase() )){
-                    return pin
-                }
-            }).map( (pin, i) => {
-                return (
-                    <li>{pin.title}</li>
-                )
-            })
-
-
-        // const pins = Object.values(this.props.pins).map( (pin, i) => {
-        //     return <li key={i}>{pin.title}</li>
-        // })
-
         
         return (
             <div>
@@ -56,9 +37,6 @@ class Searchbar extends React.Component{
                             placeholder="Search" 
                             onChange={this.handleInput}/>
                 </form>
-                <ul>
-                    {pins}
-                </ul>
             </div>
         )
     }
