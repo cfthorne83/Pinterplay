336794aeaff2f1e074cab819f68a9739d7b2d1f1
cthorne <cfthorne83@gmail.com>
2021-05-22 09:15:13 -0400

    add dorothy to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,46 +26,46 @@ pink = Board.create(
                 description: "Pink!", 
                 user_id: demo_user.id   
             )
-dark = Board.create(
-                title: "Dark", 
-                description: "Shadow!", 
-                user_id: demo_user.id   
-            )
 gold = Board.create(
                 title: "Gold", 
                 description: "Gold!", 
                 user_id: demo_user.id   
             )
+dark = Board.create(
+                title: "Black", 
+                description: "Shadow!", 
+                user_id: demo_user.id   
+            )
 
-boards = [blue, pink, dark, gold]
+boards = [blue, pink, gold, dark]
 nums = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen"]
 
 arr = []
-(0..1).each do |board_i|
-    (0..1).each do |i| 
-i = 0
-        if (board_i < 3) && (i > 9)
-            next
-        else
-            board = boards[board_i]
-                if board != dark
-                    initial = board.title[0].downcase + i.to_s
-                    title = board.title + " " + nums[i]
-                else 
-                    initial = "d"
-                    title = "Black " + nums[i]
-                end
-            uri = "https://mypin-seeds.s3.amazonaws.com/#{initial}.jpg"
+(0..3).each do |board_i|
+    (0..9).each do |i| 
+        # if (board_i < 3) && (i > 9)
+        #     next
+        # else
 
-            file = URI.open(uri)
-            pin = Pin.create(title: title, board_id: board.id, user_id: demo_user.id)
-            pin.photo.attach(io: file, filename: '#{initial}.jpg')
-            pin.save
+        board = boards[board_i]
+        if board != dark
+            initial = board.title[0].downcase + i.to_s
+            title = board.title + " " + nums[i]
+        else 
+            initial = "d"
+            title = "Black " + nums[i]
         end
+
+        uri = "https://mypin-seeds.s3.amazonaws.com/#{initial}.jpg"
+        file = URI.open(uri)
+        pin = Pin.create(title: title, board_id: board.id, user_id: demo_user.id)
+        pin.photo.attach(io: file, filename: '#{initial}.jpg')
+        pin.save
+        # end
     end
 end
 
-chris_p_bacon = User.create(
+dorothy = User.create(
                         fname: "Dorothy", 
                         lname: "Zbornak", 
                         username:"Dorothy", 
@@ -79,12 +79,25 @@ light = Board.create(
                 user_id: demo_user.id   
             )
 
-bacon_file1 = URI.open("https://mypin-seeds.s3.amazonaws.com/bacon1.jpg")
-bacon_pin1 = Pin.create(title: "bacon one", board_id: bacon.id, user_id: chris_p_bacon.id)
-bacon_pin1.photo.attach(io: bacon_file1, filename: 'bacon1.jpg')
-bacon_pin1.save
+(0..9).each do |i| 
 
-bacon_file2 = URI.open("https://mypin-seeds.s3.amazonaws.com/+bacon2.jpg")
-bacon_pin2 = Pin.create(title: "bacon two", board_id: bacon.id, user_id: chris_p_bacon.id)
-bacon_pin2.photo.attach(io: bacon_file2, filename: 'bacon2.jpg')
-bacon_pin2.save
+    board = light
+    initial = board.title[0].downcase + i.to_s
+    title = board.title + " " + nums[i]
+    
+    uri = "https://mypin-seeds.s3.amazonaws.com/#{initial}.jpg"
+    file = URI.open(uri)
+    pin = Pin.create(title: title, board_id: board.id, user_id: dorothy.id)
+    pin.photo.attach(io: file, filename: '#{initial}.jpg')
+    pin.save
+end
+
+# bacon_file1 = URI.open("https://mypin-seeds.s3.amazonaws.com/bacon1.jpg")
+# bacon_pin1 = Pin.create(title: "bacon one", board_id: bacon.id, user_id: chris_p_bacon.id)
+# bacon_pin1.photo.attach(io: bacon_file1, filename: 'bacon1.jpg')
+# bacon_pin1.save
+
+# bacon_file2 = URI.open("https://mypin-seeds.s3.amazonaws.com/+bacon2.jpg")
+# bacon_pin2 = Pin.create(title: "bacon two", board_id: bacon.id, user_id: chris_p_bacon.id)
+# bacon_pin2.photo.attach(io: bacon_file2, filename: 'bacon2.jpg')
+# bacon_pin2.save
