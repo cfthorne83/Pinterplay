b147dcb37354fa8635a214490f93603428345903
cthorne <cfthorne83@gmail.com>
2021-05-27 15:41:26 -0400

    forgot to save

--- frontend/components/pins/pin_show.jsx
+++ frontend/components/pins/pin_show.jsx
@@ -110,10 +110,10 @@ class PinShow extends React.Component {
         })
         if (this.props.pin.user && this.props.pin.user.id !== this.props.currentUser.id) {
             if (followIds.includes(this.props.pin.user.id)){
-                this.setState({ following: true})
+                this.setState({ following: true});
                 return <button onClick={this.handleUnfollow}>Unfollow</button>
             } else {
-                this.setState({ following: false})
+                this.setState({ following: false});
                 return <button onClick={this.handleFollow}>Follow</button>
             }
         }
