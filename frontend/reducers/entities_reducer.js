002266f73bde4f51f12b23db2326f90bccffc3a1
cthorne <cfthorne83@gmail.com>
2021-05-27 17:12:58 -0400

    forgot save

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import boardsReducer from './boards_reducer';
 import pinsReducer from './pins_reducer';
-import followsReducer from './follows_reducer'
+import followsReducer from './follows_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
