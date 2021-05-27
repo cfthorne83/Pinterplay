6aa69363c0d80300d4030be3b27c4e2325277305
cthorne <cfthorne83@gmail.com>
2021-05-27 16:37:43 -0400

    finish follow actions

--- frontend/actions/follow_actions.js
+++ frontend/actions/follow_actions.js
@@ -9,3 +9,26 @@ export const receiveFollow = (follow) => {
         follow
     }
 }
+
+export const removeFollow = (followId) => {
+    return {
+        type: REMOVE_FOLLOW,
+        followId
+    }
+}
+
+export const createFollow = (follower_id, followed_id) => {
+    return dispatch => {
+        return FollowApiUtil.createFollow(follower_id, followed_id).then( follow => {
+            dispatch(receiveFollow(follow))
+        })
+    }
+}
+
+export const deleteFollow = (follower_id, followed_id) => {
+    return dispatch => {
+        return FollowApiUtil.deleteFollow(follower_id, followed_id).then( followId => {
+            dispatch(receiveFollow(followId))
+        })
+    }
+}
