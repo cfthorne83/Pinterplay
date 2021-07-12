71c54602a419477743c3653206fe8b224cc3dcb1
cthorne <cfthorne83@gmail.com>
2021-07-12 14:16:24 -0400

    remove conditionals in pin index render

--- frontend/components/pins/pin_index.jsx
+++ frontend/components/pins/pin_index.jsx
@@ -63,11 +63,12 @@ class PinIndex extends React.Component {
                     <h1>Retrieving Pins</h1>
                 </div>
             )
-        } else if ( this.props.board && this.props.pins.length === 0 && this.state.loading === false ) {
-            return <h1 className="pin-index-empty">This board is empty, upload a pin!</h1>
-        } else if ( !this.props.following && this.props.pins.length === 0 && this.state.loading === false ){
-            return <h1 className="pin-index-empty">There are no pins in the database:(</h1>
-        }
+        } 
+        // else if ( this.props.board && this.props.pins.length === 0 && this.state.loading === false ) {
+        //     return <h1 className="pin-index-empty">This board is empty, upload a pin!</h1>
+        // } else if ( !this.props.following && this.props.pins.length === 0 && this.state.loading === false ){
+        //     return <h1 className="pin-index-empty">There are no pins in the database:(</h1>
+        // }
 
         let {pins, following} = this.props;
 
