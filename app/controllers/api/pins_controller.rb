10b784ab1626adbd45ce916b3b5335a38daed641
cthorne <cfthorne83@gmail.com>
2021-07-12 14:04:28 -0400

    add limit to pins index

--- app/controllers/api/pins_controller.rb
+++ app/controllers/api/pins_controller.rb
@@ -20,7 +20,7 @@ class Api::PinsController < ApplicationController
             @limit = true
             @pins = user.pins.limit(3).shuffle
         else
-            @pins = Pin.all
+            @pins = Pin.all.limit(5)
         end
         if @pins 
             render :index
