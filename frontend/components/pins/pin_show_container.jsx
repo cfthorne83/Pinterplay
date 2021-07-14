252a024a7601f3a9db99d0571b9c282f132fd7b1
cthorne <cfthorne83@gmail.com>
2021-07-14 13:53:51 -0400

    delete dead code pin show

--- frontend/components/pins/pin_show_container.jsx
+++ frontend/components/pins/pin_show_container.jsx
@@ -18,7 +18,6 @@ const msp = (state, ownProps) => {
         newPin: { board_id: "" },
         shuffle: ownProps.shuffle,
         currentUser: state.entities.users[state.session.id], 
-        // following: state.entities.users[state.session.id].following
         following: following
     }
 }
