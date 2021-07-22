b56a7e3e47eb4b49450f1560e2a2566d9022aad6
cthorne <cfthorne83@gmail.com>
2021-07-22 16:19:13 -0400

    modify the user actions and reducer;

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,18 +1,27 @@
 import * as UserApiUtil from "../util/user.api.util";
 
 export const RECEIVE_USER = 'RECEIVE_USER';
+import { receiveCurrentUser } from "./session_actions";
 
-export const receiveUser = (user) => {
-    return {
-        type: RECEIVE_USER,
-        user
-    }
-};
+// export const receiveUser = (user) => {
+//     return {
+//         type: RECEIVE_USER,
+//         user
+//     }
+// };
+
+// export const updateUser = (user) => {
+//     return dispatch => {
+//         return UserApiUtil.updateUser(user).then( user => {
+//             dispatch(receiveUser(user))
+//         })
+//     }
+// }
 
 export const updateUser = (user) => {
     return dispatch => {
         return UserApiUtil.updateUser(user).then( user => {
-            dispatch(receiveUser(user))
+            dispatch(receiveCurrentUser(user))
         })
     }
 }
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -23,8 +23,8 @@ const usersReducer = (state = {}, action) => {
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
-    case RECEIVE_USER:
-        return Object.assign({}, state, { [action.user.id]: action.user });
+    // case RECEIVE_USER:
+    //     return Object.assign({}, state, { [action.user.id]: action.user });
     default:
       return state;
   }
