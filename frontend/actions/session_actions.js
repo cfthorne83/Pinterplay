ad626cce246a39c19c1c83f614f7556d34fbc8ee
cthorne <cfthorne83@gmail.com>
2021-07-22 16:18:52 -0400

    f

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,75 +1,117 @@
+// import * as APIUtil from '../util/session_api_util';
+
+// export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+// export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+// export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+
+// export const receiveCurrentUser = currentUser => {
+//     return {
+//         type: RECEIVE_CURRENT_USER,
+//         currentUser
+//     }
+// };
+
+// export const logoutCurrentUser = () => {
+//     return {
+//         type: LOGOUT_CURRENT_USER
+//     }
+// };
+
+// export const receiveErrors = errors => {
+//     return {
+//         type: RECEIVE_SESSION_ERRORS,
+//         errors
+//     }
+// };
+
+// // export const signup = user => {
+// //     return (dispatch) => {
+// //         return APIUtil.signup(user).then(user => {
+// //             dispatch(receiveCurrentUser(user)).then(err => {
+// //                 dispatch(receiveErrors(err.resposeJSON))
+// //             })
+// //         })
+// //     }
+// // };
+// export const signup = user => dispatch => (
+//     APIUtil.signup(user).then(user => (
+//         dispatch(receiveCurrentUser(user))
+//     ), err => (
+//         dispatch(receiveErrors(err.responseJSON))
+//     ))
+// );
+
+// // export const login = user => {
+// //     return (dispatch) => {
+// //         const receiveErrorCB = err => {
+// //             dispatch(receiveErrors(err.resposeJSON))
+// //         }
+// //         const receiveUserCB = user => {
+// //             return dispatch(receiveCurrentUser(user))
+// //         }
+
+// //         return APIUtil.login(user).then(
+// //             receiveUserCB, receiveErrorCB
+// //         )
+// //     }
+// // };
+// export const login = user => dispatch => (
+//     APIUtil.login(user).then(user => (
+//         dispatch(receiveCurrentUser(user))
+//     ), err => (
+//         dispatch(receiveErrors(err.responseJSON))
+//     ))
+// );
+
+// // export const logout = () => {
+// //     return (dispatch) => {
+// //         return APIUtil.logout().then(user => {
+// //             dispatch(logoutCurrentUser())
+// //         })
+// //     }
+// // };
+// export const logout = () => dispatch => (
+//   APIUtil.logout().then(user => (
+//     dispatch(logoutCurrentUser())
+//   ))
+// );
+
 import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
-export const receiveCurrentUser = currentUser => {
-    return {
-        type: RECEIVE_CURRENT_USER,
-        currentUser
-    }
-};
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
 
-export const logoutCurrentUser = () => {
-    return {
-        type: LOGOUT_CURRENT_USER
-    }
-};
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
 
-export const receiveErrors = errors => {
-    return {
-        type: RECEIVE_SESSION_ERRORS,
-        errors
-    }
-};
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
 
-// export const signup = user => {
-//     return (dispatch) => {
-//         return APIUtil.signup(user).then(user => {
-//             dispatch(receiveCurrentUser(user)).then(err => {
-//                 dispatch(receiveErrors(err.resposeJSON))
-//             })
-//         })
-//     }
-// };
 export const signup = user => dispatch => (
-    APIUtil.signup(user).then(user => (
-        dispatch(receiveCurrentUser(user))
-    ), err => (
-        dispatch(receiveErrors(err.responseJSON))
-    ))
+  APIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
 );
 
-// export const login = user => {
-//     return (dispatch) => {
-//         const receiveErrorCB = err => {
-//             dispatch(receiveErrors(err.resposeJSON))
-//         }
-//         const receiveUserCB = user => {
-//             return dispatch(receiveCurrentUser(user))
-//         }
-
-//         return APIUtil.login(user).then(
-//             receiveUserCB, receiveErrorCB
-//         )
-//     }
-// };
 export const login = user => dispatch => (
-    APIUtil.login(user).then(user => (
-        dispatch(receiveCurrentUser(user))
-    ), err => (
-        dispatch(receiveErrors(err.responseJSON))
-    ))
+  APIUtil.login(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
 );
 
-// export const logout = () => {
-//     return (dispatch) => {
-//         return APIUtil.logout().then(user => {
-//             dispatch(logoutCurrentUser())
-//         })
-//     }
-// };
 export const logout = () => dispatch => (
   APIUtil.logout().then(user => (
     dispatch(logoutCurrentUser())
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,17 +1,33 @@
-import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+// import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_USER } from '../actions/user_actions';
 
-const usersReducer = (state = {}, action) => {
-    Object.freeze(state);
+// const usersReducer = (state = {}, action) => {
+//     Object.freeze(state);
 
-    switch (action.type) {
-        case RECEIVE_CURRENT_USER:
-            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
-        case RECEIVE_USER:
-            return Object.assign({}, state, { [action.user.id]: action.user });
-        default:
-            return state;
-    }
+//     switch (action.type) {
+//         case RECEIVE_CURRENT_USER:
+//             return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+//         case RECEIVE_USER:
+//             return Object.assign({}, state, { [action.user.id]: action.user });
+//         default:
+//             return state;
+//     }
+// };
+
+// export default usersReducer;
+
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+    case RECEIVE_USER:
+        return Object.assign({}, state, { [action.user.id]: action.user });
+    default:
+      return state;
+  }
 };
 
 export default usersReducer;
