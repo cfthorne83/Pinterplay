b75a962189cbabe1a7a328ef65165688f5d15be6
cthorne <cfthorne83@gmail.com>
2021-07-22 15:53:10 -0400

    replace session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,21 +1,44 @@
-import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+// import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
 
-// const _nullUser = { id: null };
+// // const _nullUser = { id: null };
+// const _nullUser = Object.freeze({
+//   id: null
+// });
+
+// const sessionReducer = (state = _nullUser, action) => {
+//     Object.freeze(state);
+    
+//     switch (action.type) {
+//         case RECEIVE_CURRENT_USER:
+//             return { id: action.currentUser.id };
+//         case LOGOUT_CURRENT_USER:
+//             return _nullUser;
+//         default:
+//             return state;
+//     }
+// };
+
+// export default sessionReducer;
+
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER,
+} from '../actions/session_actions';
+
 const _nullUser = Object.freeze({
   id: null
 });
 
 const sessionReducer = (state = _nullUser, action) => {
-    Object.freeze(state);
-    
-    switch (action.type) {
-        case RECEIVE_CURRENT_USER:
-            return { id: action.currentUser.id };
-        case LOGOUT_CURRENT_USER:
-            return _nullUser;
-        default:
-            return state;
-    }
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { id: action.currentUser.id };
+    case LOGOUT_CURRENT_USER:
+      return _nullUser;
+    default:
+      return state;
+  }
 };
 
 export default sessionReducer;
