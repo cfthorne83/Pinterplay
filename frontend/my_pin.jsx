8bad12a879d98eba8a051491bcbe160b2e833b5d
cthorne <cfthorne83@gmail.com>
2021-07-25 16:14:32 -0400

    paste code to entry;

--- frontend/my_pin.jsx
+++ frontend/my_pin.jsx
@@ -5,18 +5,31 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // let store;
+    // if (window.currentUser) {
+    //     const preloadedState = {
+    //         entities: {
+    //             users: { [window.currentUser.id]: window.currentUser }
+    //         },
+    //         session: { id: window.currentUser.id }
+    //     };
+    //     store = configureStore(preloadedState);
+    //     delete window.currentUser;
+    // } else {
+    //     store = configureStore();
+    // }
     let store;
     if (window.currentUser) {
-        const preloadedState = {
-            entities: {
-                users: { [window.currentUser.id]: window.currentUser }
-            },
-            session: { id: window.currentUser.id }
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
+      const preloadedState = {
+        entities: {
+          users: { [window.currentUser.id]: window.currentUser }
+        },
+        session: { id: window.currentUser.id }
+      };
+      store = configureStore(preloadedState);
+      delete window.currentUser;
     } else {
-        store = configureStore();
+      store = configureStore();
     }
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store}/>, root);
