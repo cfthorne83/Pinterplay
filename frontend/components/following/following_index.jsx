6758f0a35f57f8c3913cc7e724180e80458a1fc4
cthorne <cfthorne83@gmail.com>
2021-08-11 14:43:53 -0400

    add unfollow to following modal

--- frontend/components/following/following_index.jsx
+++ frontend/components/following/following_index.jsx
@@ -2,12 +2,23 @@ import React from "react";
 
 const Following = (props) => {
     const follows = props.following.map( follow => {
+        debugger
         return (
                 <li>
                     <span>      
                         {follow.username}
                     </span>                    
-                    <button>Unfollow</button>
+                    <button onClick={ () => ($.ajax ({
+                                                        url: "/api/friendships",
+                                                        method: "DELETE",
+                                                        data: {
+                                                            follower_id: props.currentUser.id,
+                                                            followed_id: follow.id
+                                                        }
+                                                    })
+                                            )
+                                    }
+                    >Unfollow</button>
                 </li>
         )
     })
@@ -22,3 +33,20 @@ const Following = (props) => {
 }
 
 export default Following;
+
+// handleFollow() {
+//         $.ajax ({
+//                     url: "/api/friendships",
+//                     method: "POST",
+//                     data: {
+//                         follower_id: this.props.currentUser.id,
+//                         followed_id: this.props.pin.user.id
+//                     }
+//                 })
+                
+//         // this.props.createFollow(this.state.friendship)
+//                 .then(this.setState({ 
+//                                         followIds: this.state.followIds.concat([this.props.pin.user.id]),
+//                                         followers: this.state.followers + 1
+//                                     }))
+//     }
--- frontend/components/following/following_index_container.jsx
+++ frontend/components/following/following_index_container.jsx
@@ -2,17 +2,38 @@ import { connect } from "react-redux";
 
 import Following from "./following_index";
 
+// import { createFollow, deleteFollow } from "../../actions/follow_actions";
 import { closeModal } from "../../actions/modal_actions";
 
 const msp = (state, ownProps) => {
     return {
-        following: state.ui.modal.options
+        following: state.ui.modal.options,
+        currentUser: state.entities.users[state.session.id]
     }
 }
 
 const mdp = (dispatch, ownProps) => {
+    // $.ajax ({
+    //                 url: "/api/friendships",
+    //                 method: "POST",
+    //                 data: {
+    //                     follower_id: this.props.currentUser.id,
+    //                     followed_id: this.props.pin.user.id
+    //                 }
+    //             })
     return  {
-        closeModal: () => dispatch(closeModal())
+        closeModal: () => dispatch(closeModal()),
+        deleteFollow: (friendship) => {
+            return (
+                $.ajax ({
+                    url: "/api/friendships",
+                    method: "POST",
+                    data: {
+                        friendship
+                    }
+                })
+            )
+        }
     }
 }
 
