a6363d1f4e128c3a8c4fb73f817897d49689f41e
cthorne <cfthorne83@gmail.com>
2021-08-15 11:17:35 -0400

    add follower friendship index

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -6,6 +6,7 @@ class Api::FriendshipsController < ApplicationController
     def index 
         @user = current_user
         @follows = @user.following
+        @followers = @user.followers
         render :index
     end
 
