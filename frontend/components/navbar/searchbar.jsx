2177749a62ac4ea295c07c0df665d665ddc06103
cthorne <cfthorne83@gmail.com>
2021-08-20 14:24:34 -0400

    change searchbar input to search

--- frontend/components/navbar/searchbar.jsx
+++ frontend/components/navbar/searchbar.jsx
@@ -7,7 +7,6 @@ class Searchbar extends React.Component{
 
         this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.button = this.button.bind(this);
     }
 
     handleInput(e) {
@@ -22,31 +21,24 @@ class Searchbar extends React.Component{
         e.preventDefault();
         // debugger
         // this.props.searchInput(this.state.searchTerm);
-        this.setState({
-            searchInput: ""
-        })
+        // this.setState({
+        //     searchInput: ""
+        // })
     }
 
-    button() {
-        this.setState({
-            searchInput: ""
-        })
-    }
+    render() { 
 
-    render() {
-        debugger
         return (
             <div className='searchbar-outer'>
                 <form className='searchbar-container'
                 onSubmit={this.handleSubmit}
                 >
                     <input  className='searchbar' 
-                            type="text" 
+                            type="search" 
                             placeholder="Search"
                             value={this.state.searchInput}
                             onChange={this.handleInput}/>
                 </form>
-                <button onClick={this.button}>alskdjf;lsakjdfl;kasas;dlfkasldfjkjewrsß</button>
             </div>
         )
     }
