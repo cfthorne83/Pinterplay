1f8d5e67bf62c0d5c80acf28a73826133651ed4b
cthorne <cfthorne83@gmail.com>
2021-08-21 12:30:22 -0400

    f

--- frontend/components/navbar/searchbar.jsx
+++ frontend/components/navbar/searchbar.jsx
@@ -7,19 +7,31 @@ class Searchbar extends React.Component{
 
         this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleInput(e) {
+    handleClick(e) {
         this.setState({
-            searchTerm: e.currentTarget.value
+            searchTerm: ""
         })
-        this.props.searchInput(e.currentTarget.value);
+    }
+
+    // handleChange(e) {
+    //     debugger
+    //     // this.props.searchInput("");
+    // }
+
+    handleInput(e) {
+        // this.setState({
+        //     searchTerm: e.currentTarget.value
+        // })
+        // this.props.searchInput(e.currentTarget.value);
     }
     
     handleSubmit(e) {
         e.preventDefault();
-        const searchbar = document.querySelector(".searchbar");
-        searchbar.blur();
+        // const searchbar = document.querySelector(".searchbar");
+        // searchbar.blur();
         // this.props.searchInput(this.state.searchTerm);
         // this.setState({
         //     searchInput: ""
@@ -31,13 +43,19 @@ class Searchbar extends React.Component{
         return (
             <div className='searchbar-outer'>
                 <form className='searchbar-container'
-                onSubmit={this.handleSubmit}>
+                    onSubmit={this.handleSubmit}
+                >
                     <input  className='searchbar' 
-                            type="search" 
+                            type="input" 
                             placeholder="Search"
                             value={this.state.searchInput}
-                            onChange={this.handleInput}/>
+                            onInput={this.handleInput}
+                            // onChange={this.handleChange}
+                            // onClick={this.handleClick}
+                            />
                 </form>
+                <button style={{fontSize: "20px"}}
+                    onClick={this.handleClick}>X</button>
             </div>
         )
     }
