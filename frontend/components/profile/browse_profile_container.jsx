873c998176d2fe6d6ca53cdec665345c4ce5ce0c
cthorne <cfthorne83@gmail.com>
2021-08-23 18:34:24 -0400

    delete dead code

--- frontend/components/profile/browse_profile_container.jsx
+++ frontend/components/profile/browse_profile_container.jsx
@@ -3,36 +3,18 @@ import { openModal, closeModal } from '../../actions/modal_actions';
 import { fetchUser, fetchUsers } from '../../actions/user_actions';
 import { fetchFollows } from '../../actions/follow_actions';
 
-
 import BrowseProfile from './browse_profile';
 
 const mSTP = (state, ownProps) => {
-    // let followers = [];
-    // let following = [];
-    // if (state.entities.follows.followers && state.entities.follows.following){
-    //     followers = Object.values(state.entities.follows.followers);
-    //     following = Object.values(state.entities.follows.following);
-    // } else if (state.entities.follows.followers && !state.entities.follows.following) {
-    //     followers = Object.values(state.entities.follows.followers);
-    // } else if (state.entities.follows.following && !state.entities.follows.followers) {
-    //     following = Object.values(state.entities.follows.following);
-    // }
     return {
         user: state.entities.users[ownProps.match.params.userId],
-        // currentUser: state.entities.follows.followers[ownProps.match.params.userId],
-        // currentUser: state.entities.users[state.session.id],
-        // lastBoard: Object.values(state.entities.boards)[0],
-        // following: following,
-        // followers: followers
     };
 };
 
 const mDTP = (dispatch, ownProps) => {
     return {
-        // fetchUser: (userId) => dispatch(fetchUser(userId)),
         openModal: (modal, options) => dispatch(openModal(modal, options)),
         closeModal: () => dispatch(closeModal()),
-        // fetchFollows: (userId) => dispatch(fetchFollows(userId)),
         fetchUsers: () => dispatch(fetchUsers()) 
     };
 };
