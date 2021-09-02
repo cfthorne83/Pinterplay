ce32a7d84682d536be9473dd68a5e09692d7167d
cthorne <cfthorne83@gmail.com>
2021-09-02 17:36:54 -0400

    debub redirect after pin create

--- frontend/components/pins/pin_show_link_container.jsx
+++ frontend/components/pins/pin_show_link_container.jsx
@@ -4,10 +4,12 @@ import PinShowLink from "./pin_show_link";
 import { closeModal } from "../../actions/modal_actions";
 
 const msp = (state, ownProps) => {
+    debugger
     return {
         pinId: state.ui.modal.options.id, 
         board: state.ui.modal.options.board,
-        image: state.ui.modal.options.image
+        image: state.ui.modal.options.image,
+        currentUser: state.entities.users[state.session.id]
     }
 }
 
